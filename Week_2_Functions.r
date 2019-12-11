0305bf5e3b40d684b928a95bca6dc2e64b5b0259
chris-john-shaw <chris.shaw@realitymine.com>
2019-12-11 09:45:40 +0000

    Added new file for Week 2 fuctions, creating basic funtions and a function utilising a for loop

--- Week_2_Functions.r
+++ Week_2_Functions.r
@@ -1,4 +1,4 @@
-##Create a functon
+##Create a basic functon
 
 add2 <- function(x, y) {
         x + y
@@ -11,3 +11,17 @@ above10 <- function(x) {
 }
 
 add2(1, 2)
+
+above10(11)
+
+
+##Function with for loop to caluclate mean
+
+columnmean <- function(y, removeNA = TRUE) {
+        nc <- ncol(y)
+        means <- numeric(nc)
+        for(i in 1:nc) {
+                  means[i] <- mean(y[,i], na.rm = removeNA)
+        }
+        means
+}
